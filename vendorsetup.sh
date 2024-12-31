cc22bd8bebef6b1af1c9f0febf264ad75194e9d0
oliveiraleo <oliveiraleo@users.noreply.github.com>
2024-12-31 11:55:05 -0300

    vendorsetup: Update patch sources to QPR3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,66 +32,8 @@ patch -p1 <0001-Set-Product-Compressed-Apex-false.patch
 cd ../..
 
 echo 'Adding patch to Frameworks base'
-cd frameworks/base
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0001-hwui-reset-to-android-13-0-0-r13.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0002-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0003-Ignore-cgroup-creation-errors.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0004-Dont-use-stretch-effect-by-default.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0005-Dont-use-patterned-style-ripple-effect-by-default.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0006-Implement-activity-boost-optimization.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0007-Implement-transition-boosting.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0008-Boost-gesture-flings.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0009-Implement-exit-app-animation-boost.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0010-QS-fling-animation-boost.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0011-Optimize-connected-battery-level-scanning.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0012-Optimize-window-transition-animation-scaling.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0013-Disable-vendor-mismatch-warning.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0014-allow-to-tune-killing-cached-processes-until-post-boot-completed.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0015-Optimize-AbsListView-to-reduce-click-operation-latency.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0016-Revert-Remove-deprecated-Radio-1-4-Apı-and-references.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0017-SettingsProvider-Resolve-google-gms-configurator-denials.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0018-B-service-aging-propagation-on-memory-pressure.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0019-LightsService-Mute-an-annoying-error-message.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0020-CachedAppOptimizer-revert-freezer-to-cgroups-v1.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0021-BiometricScheduler-Cancel-operation-if-not-idle.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0022-Fix-exception-when-retrieving-target-SDK-version.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0023-Keystore-Spoof-locked-bootloader-on-local-attestations.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0024-Spoof-build-fingerprint-for-Google-Play-Services.patch
-# wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0025-Update-Spoof-fingerprint-patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0026-fixup-hwui-reset-to-android-13-0-0-r13.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0027-Disable-deprecated-target-abi-dialog.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0028-Disable-FP-lockouts.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/framework_base/0029-Optimize-notification-expansion-animation.patch
-patch -p1 <0001-hwui-reset-to-android-13-0-0-r13.patch
-patch -p1 <0002-BootReceiver-Return-early-if-trace-pipe-doesnt-exists.patch
-patch -p1 <0003-Ignore-cgroup-creation-errors.patch
-patch -p1 <0004-Dont-use-stretch-effect-by-default.patch
-patch -p1 <0005-Dont-use-patterned-style-ripple-effect-by-default.patch
-patch -p1 <0006-Implement-activity-boost-optimization.patch
-patch -p1 <0007-Implement-transition-boosting.patch
-patch -p1 <0008-Boost-gesture-flings.patch
-patch -p1 <0009-Implement-exit-app-animation-boost.patch
-patch -p1 <0010-QS-fling-animation-boost.patch
-patch -p1 <0011-Optimize-connected-battery-level-scanning.patch
-patch -p1 <0012-Optimize-window-transition-animation-scaling.patch
-patch -p1 <0013-Disable-vendor-mismatch-warning.patch
-patch -p1 <0014-allow-to-tune-killing-cached-processes-until-post-boot-completed.patch
-patch -p1 <0015-Optimize-AbsListView-to-reduce-click-operation-latency.patch
-patch -p1 <0016-Revert-Remove-deprecated-Radio-1-4-Apı-and-references.patch
-patch -p1 <0017-SettingsProvider-Resolve-google-gms-configurator-denials.patch
-patch -p1 <0018-B-service-aging-propagation-on-memory-pressure.patch
-patch -p1 <0019-LightsService-Mute-an-annoying-error-message.patch
-patch -p1 <0020-CachedAppOptimizer-revert-freezer-to-cgroups-v1.patch
-patch -p1 <0021-BiometricScheduler-Cancel-operation-if-not-idle.patch
-patch -p1 <0022-Fix-exception-when-retrieving-target-SDK-version.patch
-patch -p1 <0023-Keystore-Spoof-locked-bootloader-on-local-attestations.patch
-patch -p1 <0024-Spoof-build-fingerprint-for-Google-Play-Services.patch
-# patch -p1 <0025-Update-Spoof-fingerprint-patch
-patch -p1 <0026-fixup-hwui-reset-to-android-13-0-0-r13.patch
-patch -p1 <0027-Disable-deprecated-target-abi-dialog.patch
-patch -p1 <0028-Disable-FP-lockouts.patch
-patch -p1 <0029-Optimize-notification-expansion-animation.patch
-cd ../..
+rm -rf frameworks/base
+git clone --depth 1 https://github.com/oliveiraleo/android_frameworks_base.git -b lineage-21.0 frameworks/base
 
 echo 'Adding patch to Frameworks av'
 cd frameworks/av
@@ -114,44 +56,17 @@ patch -p1 <0002-SurfaceFlinger-Disable-SF-HWC-backpressure.patch
 cd ../..
 
 echo 'Adding patch to Frameworks telephony'
-cd frameworks/opt/telephony
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_opt_telephony/0001-Revert-Remove-deprecated-Radio-1-4-API-and-reference.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_opt_telephony/0002-Revert-Remove-deprecated-HAL-versions-for-IRadio.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_opt_telephony/0003-Pass-correct-value-to-setPreferredNetworkType-for-RIL-version-1-4.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_opt_telephony/0004-Fix-NPE-with-1-0-and-1-1-CardStatus.patch
-patch -p1 <0001-Revert-Remove-deprecated-Radio-1-4-API-and-reference.patch
-patch -p1 <0002-Revert-Remove-deprecated-HAL-versions-for-IRadio.patch
-patch -p1 <0003-Pass-correct-value-to-setPreferredNetworkType-for-RIL-version-1-4.patch
-patch -p1 <0004-Fix-NPE-with-1-0-and-1-1-CardStatus.patch
-cd ../../..
+rm -rf frameworks/opt/telephony
+git clone --depth 1 https://github.com/LineageOS-UL/android_frameworks_opt_telephony.git -b lineage-21.0 frameworks/opt/telephony
 
 echo 'Adding patch to System bpf'
-cd system/bpf
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/system_bpf/00001-Support-no-bpf-usecase.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/system_bpf/00002-fixup-Support-no-bpf-usecase.patch
-patch -p1 <00001-Support-no-bpf-usecase.patch
-patch -p1 <00002-fixup-Support-no-bpf-usecase.patch
-cd ../..
+rm -rf system/bpf
+git clone --depth 1 https://github.com/LineageOS-UL/android_system_bpf.git -b lineage-21.0 system/bpf
+
 
 echo 'Adding patch to NetworkStack modules'
-cd packages/modules/NetworkStack
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0001-Disable-parsing-netlink-events-from-kernel.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0002-Revert-BR05-Disable-doze-mode-workaround-when-ignore-blocked-uids.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0003-Revert-BR10-Skip-tcp-info-for-uids-which-blocked-by-data-saver.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0004-Revert-BR04-Skip-traffic-of-etworking-blocked-uid-in-data-stall-detection.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0005-Revert-Remove-isTcpInfoParsingSupported-and-Q-test-annotations.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0006-Requesting-tcp-info-via-netlink-socket-is-not-supported.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0007-Revert-BR13-dump-Device-Configs-to-dumpsys.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_NetworkStack/0008-Revert-BR03-2-Implement-NetworkStack-is-uid-networking-blocked-shell-command.patch
-patch -p1 <0001-Disable-parsing-netlink-events-from-kernel.patch
-patch -p1 <0002-Revert-BR05-Disable-doze-mode-workaround-when-ignore-blocked-uids.patch
-patch -p1 <0003-Revert-BR10-Skip-tcp-info-for-uids-which-blocked-by-data-saver.patch
-patch -p1 <0004-Revert-BR04-Skip-traffic-of-etworking-blocked-uid-in-data-stall-detection.patch
-patch -p1 <0005-Revert-Remove-isTcpInfoParsingSupported-and-Q-test-annotations.patch
-patch -p1 <0006-Requesting-tcp-info-via-netlink-socket-is-not-supported.patch
-patch -p1 <0007-Revert-BR13-dump-Device-Configs-to-dumpsys.patch
-patch -p1 <0008-Revert-BR03-2-Implement-NetworkStack-is-uid-networking-blocked-shell-command.patch
-cd ../../..
+rm -rf packages/modules/NetworkStack
+git clone --depth 1 https://github.com/LineageOS-UL/android_packages_modules_NetworkStack.git -b lineage-21.0 packages/modules/NetworkStack
 
 echo 'Adding patch to System netd'
 cd system/netd
@@ -186,24 +101,9 @@ patch -p1 <0006-Panic-into-recovery-rather-than-bootloader.patch
 cd ../..
 
 echo 'Adding patch to Connectivity modules'
-cd packages/modules/Connectivity
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0001-Allow-failing-to-load-bpf-programs-for-BPF-less-devices.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0002-Support-non-working-BPF-maps-on-old-BPF-less-kernel.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0003-More-bpf-errors-ignore-there-are-some-4-14-without-the-bpf-jit-file.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0004-disable-SocketNetlinkMonitor.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0005-dnsresolver-Support-no-bpf-usecase.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0006-clatcoordinator-Support-no-bpf-usecase.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0007-BpfNetMaps-Make-use-of-BpfNetMaps-safe.patch
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/packages_modules_Connectivity/0008-libnetworkstats-Make-use-of-BpfMap-safe.patch
-patch -p1 <0001-Allow-failing-to-load-bpf-programs-for-BPF-less-devices.patch
-patch -p1 <0002-Support-non-working-BPF-maps-on-old-BPF-less-kernel.patch
-patch -p1 <0003-More-bpf-errors-ignore-there-are-some-4-14-without-the-bpf-jit-file.patch
-patch -p1 <0004-disable-SocketNetlinkMonitor.patch
-patch -p1 <0005-dnsresolver-Support-no-bpf-usecase.patch
-patch -p1 <0006-clatcoordinator-Support-no-bpf-usecase.patch
-patch -p1 <0007-BpfNetMaps-Make-use-of-BpfNetMaps-safe.patch
-patch -p1 <0008-libnetworkstats-Make-use-of-BpfMap-safe.patch
-cd ../../..
+rm -rf packages/modules/Connectivity
+git clone --depth 1 https://github.com/LineageOS-UL/android_packages_modules_Connectivity.git -b lineage-21.0 packages/modules/Connectivity
+
 
 echo 'Adding patch to DnsResolver'
 cd packages/modules/DnsResolver
@@ -236,10 +136,11 @@ patch -p1 <0001-Add-a-preference-to-battery-optimization-page.patch
 cd ../../..
 
 echo 'Adding patch to Vendor lineage'
-cd vendor/lineage
-wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/vendor_lineage/0001-Restore-target-process-sdk-version-ovveride.patch
-patch -p1 <0001-Restore-target-process-sdk-version-ovveride.patch
-cd ../..
+rm -rf vendor/lineage
+# wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/vendor_lineage/0001-Restore-target-process-sdk-version-ovveride.patch
+# patch -p1 <0001-Restore-target-process-sdk-version-ovveride.patch
+# cd ../..
+git clone --depth 1 https://github.com/LineageOS-UL/android_vendor_lineage.git -b lineage-21.0 vendor/lineage
 
 echo 'Adding FaceUnlock Support'
 cd frameworks/base
